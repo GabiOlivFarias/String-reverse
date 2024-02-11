8e547745ec44913e74428906a4c4c717cd576f44
Gabi Farias <gabijk1234row@gmail.com>
2024-02-10 22:55:55 -0400

    [UPDATE] fucntion to use reverse

--- src/components/globals/input/PhraseInput.vue
+++ src/components/globals/input/PhraseInput.vue
@@ -5,18 +5,30 @@
       type="text"
       placeholder="Digite uma frase"
       class="size-12 w-56 cursor-text p-4 m-2 appearance-none border-2 border-solid border-primary"
-      @input="onInput"  
+      v-model="inputPhrase"  
     />
-    <ReverseButton />
+    <button @click="reversePhrase" class="m-2 p-2 text-2xl text-white bg-medium-red w-32 rounded-md flex flex-row hover:drop-shadow-red-1">
+      Reverter
+    </button>
   </div>
 </template>
 
 <script>
-import ReverseButton from '../buttons/ReverseButton.vue'
-
 export default {
-  components: {
-    ReverseButton
+  data() {
+    return {
+      inputPhrase: ''
+    }
+  },
+  methods: {
+    reversePhrase() {
+      const phraseArray = this.inputPhrase.split('')
+      let reversedPhrase = '';
+      for (let i = phraseArray.length - 1; i >= 0; i--) {
+        reversedPhrase += phraseArray[i]
+      }
+      this.inputPhrase = reversedPhrase
+    }
   }
 }
 </script>
