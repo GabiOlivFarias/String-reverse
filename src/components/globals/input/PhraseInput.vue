de0ccb611efdc10a43fd365f13a853fc07fc81bd
Gabi Farias <gabijk1234row@gmail.com>
2024-02-10 23:02:27 -0400

    [UPDATE] cleaning input and adjustments in CSS

--- src/components/globals/input/PhraseInput.vue
+++ src/components/globals/input/PhraseInput.vue
@@ -4,12 +4,15 @@
       id="phrase"
       type="text"
       placeholder="Digite uma frase"
-      class="size-12 w-56 cursor-text p-4 m-2 appearance-none border-2 border-solid border-primary"
+      class="size-12 w-96 cursor-text p-4 m-2 appearance-none border-2 border-solid border-primary"
       v-model="inputPhrase"  
     />
     <button @click="reversePhrase" class="m-2 p-2 text-2xl text-white bg-medium-red w-32 rounded-md flex flex-row hover:drop-shadow-red-1">
       Reverter
     </button>
+  </div>
+  <div v-if="reversedPhrase" class="p-2 m-2 result-container font-medium text-2xl">
+    <h3>Frase revertida: {{ reversedPhrase }}</h3>
   </div>
 </template>
 
@@ -17,17 +20,19 @@
 export default {
   data() {
     return {
-      inputPhrase: ''
-    }
+      inputPhrase: '',
+      reversedPhrase: ''
+    };
   },
   methods: {
     reversePhrase() {
       const phraseArray = this.inputPhrase.split('')
-      let reversedPhrase = '';
+      let reversedPhrase = ''
       for (let i = phraseArray.length - 1; i >= 0; i--) {
-        reversedPhrase += phraseArray[i]
+        reversedPhrase += phraseArray[i];
       }
-      this.inputPhrase = reversedPhrase
+      this.reversedPhrase = reversedPhrase
+      this.inputPhrase = ''
     }
   }
 }
